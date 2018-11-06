19c0723b2459f041cbc109740040504a87b1b9b6
Tom Stevens <tom@stevens.se>
2018-11-06 10:20:07 +0100

    Fix error about expr not defined.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
   Main app file require your start files from here. 
   This will automatically require all index.* files in the src folder
 */
-require.context(
+function importAll (r) {
+  r.keys().forEach(r);
+}
+
+importAll(require.context(
   "./src", // context folder
   true, // include subdirectories
   /index.(js|styl|less|sass|scss)/ // RegExp
-)("./" + expr + "");
+))
